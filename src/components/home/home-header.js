62c3fb2ce16bfb65d9189143bc5d0ecf69573d7c
hakeemsalman <salman74163@gmail.com>
2024-11-11 01:44:47 +0530

    added home header

--- src/components/home/home-header.js
+++ src/components/home/home-header.js
@@ -4,15 +4,19 @@ import React from 'react'
 
 export default function HomeHeader() {
   return (
-    <div className='flex flex-col items-center pt-10 text-white gap-2  min-h-[calc(100vh-70px)]'>
-      <h1 className='text-3xl'>Welcome to Talatech</h1>
-      <p>Ready to Elevate Your Business with expert Freelance Solutions? Look No Further!</p>
-      <p>In dolor laborum sunt veniam sint ea. Aliquip laboris eiusmod id cupidatat irure. Ipsum voluptate labore nulla eu minim sint deserunt velit nulla excepteur dolor aute ullamco. Nulla anim consequat cillum voluptate id aute officia. Excepteur velit qui velit laboris esse non. Ex consequat labore proident culpa ex incididunt. Ullamco ad irure amet duis tempor est minim pariatur pariatur.</p>
-      <div className='flex flex-row items-center gap-3'>
-        <Link className='text-3xl' href='/about'>About Us</Link>
-        <MoveUpRight size={30}/>
+    <div className='flex justify-center pt-20'>
+      <div className='flex flex-col items-center text-white gap-2  min-h-[calc(100vh-160px)] max-w-[1050px]'>
+        <h1 className='text-5xl'>Welcome to Talatech</h1>
+        <div className='flex flex-col gap-3 items-center'>
+          <p className='text-2xl'>Ready to Elevate Your Business with expert Freelance Solutions? Look No Further!</p>
+          <p className='text-base text-white text-center'>At Talatech Freelance Collective, we specialize in delivering high-quality, project-based services in web development, digital marketing, video editing, SEO, and cybersecurity. Whether you’re a small business or a large enterprise, we’ve got the talent and expertise to drive your growth.</p>
+          <div className='flex flex-row items-center gap-3'>
+            <Link className='text-4xl' href='/about'>About Us</Link>
+            <MoveUpRight size={32} />
+          </div>
+        </div>
       </div>
     </div>
-  
+
   )
 }
