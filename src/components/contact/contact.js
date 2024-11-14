21627c90b89e3e5196ac48ee60ed982a08738776
hakeemsalman <salman74163@gmail.com>
2024-11-14 22:51:42 +0530

    completed about

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -88,87 +88,94 @@ export default function Contact() {
   return (
     <div className='flex flex-col bg-[#F2E9E4] p-20'>
       <h2 className='font-bold text-6xl text-center'>Contact Us</h2>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div>
+      <div className='my-14' >
+        <form onSubmit={handleSubmit} className='flex flex-col gap-5 max-w-5xl' style={{margin: "auto"}}>
+          <div className='flex flex-row justify-between gap-48'>
             <input
-              className='p-3 rounded-md border-2'
+              className='p-3 rounded-md border-[#DFE4EA]'
               type="text"
               id="name"
               name="name"
               value={formData.name}
               onChange={handleChange}
               placeholder='David John'
-            />
+              />
             {errors.name && <div style={{ color: 'red' }}>{errors.name}</div>}
-          </div>
 
-          <div>
             <input
+              className='p-3 rounded-md border-[#DFE4EA]'
               type="text"
               id="service"
               name="service"
               value={formData.service}
               onChange={handleChange}
-            />
+              placeholder='Service'
+              />
             {errors.service && <div style={{ color: 'red' }}>{errors.service}</div>}
           </div>
-
-          <div>
+          <div className='flex flex-row justify-between gap-48'>
             <input
+              className='p-3 rounded-md border-[#DFE4EA]'
               type="number"
               id="duration"
               name="duration"
               value={formData.duration}
               onChange={handleChange}
-            />
+              placeholder='Duration'
+              />
             {errors.duration && <div style={{ color: 'red' }}>{errors.duration}</div>}
-          </div>
 
-          <div>
             <input
+              className='p-3 rounded-md border-[#DFE4EA]'
               type="number"
               id="budget"
               name="budget"
               value={formData.budget}
               onChange={handleChange}
-            />
+              placeholder='Budget'
+              />
             {errors.budget && <div style={{ color: 'red' }}>{errors.budget}</div>}
           </div>
 
-          <div>
+          <div className='flex flex-row justify-between gap-48'>
             <input
+              className='p-3 rounded-md border-[#DFE4EA]'
               type="text"
               id="mobile"
               name="mobile"
               value={formData.mobile}
               onChange={handleChange}
-            />
+              placeholder='Mobile Number'
+              />
             {errors.mobile && <div style={{ color: 'red' }}>{errors.mobile}</div>}
-          </div>
 
-          <div>
             <input
+              className='p-3 rounded-md border-[#DFE4EA]'
               type="email"
               id="email"
               name="email"
               value={formData.email}
               onChange={handleChange}
-            />
+              placeholder='info@example.com'
+              />
             {errors.email && <div style={{ color: 'red' }}>{errors.email}</div>}
           </div>
 
-          <div>
+          <div className='flex flex-row gap-5 items-end'>
             <textarea
+              className='p-3 rounded-md border-[#DFE4EA] w-full'
               id="message"
               name="message"
               value={formData.message}
               onChange={handleChange}
-            />
+              placeholder='info@example.com'
+              rows={5}
+              cols={100}
+              />
             {errors.message && <div style={{ color: 'red' }}>{errors.message}</div>}
+          <button className='flex items-center bg-blue-600 py-3 px-8 text-white rounded-full' type="submit">Submit</button>
           </div>
 
-          <button type="submit">Submit</button>
         </form>
 
       </div>
