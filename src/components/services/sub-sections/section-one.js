4974d9ebab888e4869753843675f01164ad583e1
hakeemsalman <salman74163@gmail.com>
2024-11-19 19:27:08 +0530

    updated services

--- src/components/services/sub-sections/section-one.js
+++ src/components/services/sub-sections/section-one.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./section-one.css"; // CSS file for styles
 import Image from "next/image";
+import Link from "next/link";
+import { MoveUpRight } from "lucide-react";
 
 export default function SectionOne() {
   return (
@@ -10,27 +12,38 @@ export default function SectionOne() {
         <Image width={400} height={400} src='/logos/mongoDB.png' alt="MongoDB Logo" className="absolute w-1/6 h-auto mongoDB" />
         <Image width={500} height={500} src='/logos/react.png' alt="React Logo" className="absolute w-1/6 h-auto react" />
         <Image width={500} height={500} src='/logos/html-css-js.png' alt="HTML/CSS Logo" className="absolute w-1/4 h-auto html-css" />
-        <Image width={500} height={500} src='/logos/ladderStraight.png' alt="Ladder" className="absolute -translate-x-1/2 w-1/6 h-auto top-[145%] left-1/2" />
-        <div className="bg-purple-600 text-white rounded-full w-72 sm:w-[550px] aspect-square flex flex-col justify-center items-center mx-auto shadow-lg shadow-purple-500/70 p-5 text-center z-10">
-          <div className="w-[390px] flex flex-col gap-5">
-            <h2 className="text-4xl font-bold">Full-stack web development</h2>
+        <div className="bg-white text-secondary rounded-full w-72 sm:w-[550px] aspect-square flex flex-col justify-center items-center mx-auto shadow-lg shadow-purple-500/70 p-5 text-center z-10 ">
+          <div className="w-[390px] flex flex-col gap-10">
+            <h2 className="text-4xl font-bold text-shadow">Full-stack web development</h2>
             <p className="text-2xl font-bold">
               Dynamic, responsive websites to take your business online or upgrade
               your current platform.
             </p>
+            <div className="flex justify-center mt-10">
+              <Link href='/services/portfolio' className="flex flex-row gap-5 px-5 py-3 items-center justify-center bg-primary rounded-md">
+                <span className="text-2xl">Learn more</span>
+                <MoveUpRight size={32} />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
 
       {/* Section 2: Professional video editing */}
       <div className="relative flex flex-col w-full h-[350px] my-36">
-        <div className="bg-purple-600 text-white rounded-full w-72 sm:w-[550px] aspect-square flex flex-col justify-center items-center mx-auto shadow-lg shadow-purple-500/70 p-5 text-center ">
-          <div className="relative w-[390px] flex flex-col gap-5">
-            <h2 className="text-4xl font-bold">Professional video editing</h2>
+        <div className="bg-white text-white rounded-full w-72 sm:w-[550px] aspect-square flex flex-col justify-center items-center mx-auto shadow-lg shadow-purple-500/70 p-5 text-center ">
+          <div className="relative text-secondary w-[390px] flex flex-col gap-5">
+            <h2 className="text-4xl font-bold text-shadow">Professional video editing</h2>
             <p className="text-2xl font-bold">
               High-quality edits for commercials, product videos, social media,
               and more.
             </p>
+            <div className="flex justify-center mt-10">
+              <Link href='/services/portfolio' className="flex flex-row gap-5 px-5 py-3 items-center justify-center bg-primary rounded-md">
+                <span className="text-2xl">Learn more</span>
+                <MoveUpRight size={32} />
+              </Link>
+            </div>
           </div>
         </div>
         <Image width={500} height={500}
@@ -43,11 +56,8 @@ export default function SectionOne() {
           alt="DaVinci Resolve Logo"
           className="absolute w-1/6 h-auto davinci"
         />
-        <Image width={500} height={500} src='/logos/ladderInclinedLeft.png' alt="Ladder" className="absolute translate-x-[85%] w-[15%] h-auto top-[100%] left-1/2" />
-
       </div>
-
-      {/* Section 3: Cybersecurity */}
+      {/* 
       <div className="relative w-full h-[350px] my-36">
         <Image width={500} height={500} src='/logos/security.png' alt="Shield Icon" className="absolute w-1/6 h-auto shield" />
         <Image width={500} height={500}
@@ -55,26 +65,39 @@ export default function SectionOne() {
           alt="Bitdefender Logo"
           className="absolute w-1/6 h-auto bitdefender"
         />
-        <Image width={500} height={500} src='/logos/ladderStraight.png' alt="Ladder" className="absolute -translate-x-1/2 w-1/6 h-auto top-[145%] left-1/2" />
-        <div className="bg-purple-600 text-white rounded-full w-72 sm:w-[550px] aspect-square flex flex-col justify-center items-center mx-auto shadow-lg shadow-purple-500/70 p-5 text-center z-10">
-          <h2 className="text-4xl font-bold">Cybersecurity audits and solutions</h2>
+        <div className="bg-white text-secondary rounded-full w-72 sm:w-[550px] aspect-square flex flex-col justify-center items-center mx-auto shadow-lg shadow-purple-500/70 p-5 text-center z-10">
+          <h2 className="text-4xl font-bold text-shadow">Cybersecurity audits and solutions</h2>
           <p className="text-2xl font-bold">
             Keep your business safe with a complete assessment of your digital
             assets.
           </p>
+          <div className="flex justify-center mt-10">
+              <Link href='/services/portfolio' className="flex flex-row gap-5 px-5 py-3 items-center justify-center bg-primary rounded-md">
+                <span className="text-2xl">Learn more</span>
+                <MoveUpRight size={32} />
+              </Link>
+            </div>
         </div>
-      </div>
+      </div> */}
 
       {/* Section 4: AWS Cloud Solutions */}
       <div className="relative w-full h-[350px] my-36">
         <Image width={500} height={500} src='/logos/aws.png' alt="AWS Logo" className="absolute w-1/6 h-auto aws" />
         <Image width={500} height={500} src='/logos/devops.png' alt="DevOps Logo" className="absolute w-1/3 h-auto devops" />
-        <Image width={500} height={500} src='/logos/ladderInclinedLeft.png' alt="Ladder" className="absolute translate-x-[85%] w-[15%] h-auto top-[120%] left-1/2" />
-        <div className="bg-purple-600 text-white rounded-full w-72 sm:w-[550px] aspect-square flex flex-col justify-center items-center mx-auto shadow-lg shadow-purple-500/70 p-5 text-center z-10">
-          <h2 className="text-4xl font-bold">AWS cloud solutions</h2>
-          <p className="text-2xl font-bold">
-            Scale your operations with secure, cost-efficient cloud technology.
-          </p>
+        <div className="bg-white text-secondary rounded-full w-72 sm:w-[550px] aspect-square flex flex-col justify-center items-center mx-auto shadow-lg shadow-purple-500/70 p-5 text-center z-10">
+          <div className="relative text-secondary w-[390px] flex flex-col gap-5">
+
+            <h2 className="text-4xl font-bold text-shadow">AWS cloud solutions</h2>
+            <p className="text-2xl font-bold">
+              Scale your operations with secure, cost-efficient cloud technology.
+            </p>
+            <div className="flex justify-center mt-10">
+              <Link href='/services/portfolio' className="flex flex-row gap-5 px-5 py-3 items-center justify-center bg-primary rounded-md">
+                <span className="text-2xl">Learn more</span>
+                <MoveUpRight size={32} />
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -82,12 +105,20 @@ export default function SectionOne() {
       <div className="relative w-full h-[350px] my-36 pb-36">
         <Image width={500} height={500} src='/logos/seo.png' alt="SEO Icon" className="absolute w-1/6 h-auto seo" />
         <Image width={500} height={500} src='/logos/man-running.png' alt="man Icon" className="absolute w-1/6 h-auto man" />
-        <div className="bg-purple-600 text-white rounded-full w-72 sm:w-[550px] aspect-square flex flex-col justify-center items-center mx-auto shadow-lg shadow-purple-500/70 p-5 text-center z-10">
-          <h2 className="text-4xl font-bold">SEO and digital marketing</h2>
-          <p className="text-2xl font-bold">
-            Optimize your web presence to rank higher on search engines and
-            boost traffic.
-          </p>
+        <div className="bg-white text-secondary rounded-full w-72 sm:w-[550px] aspect-square flex flex-col justify-center items-center mx-auto shadow-lg shadow-purple-500/70 p-5 text-center z-10">
+          <div className="relative text-secondary w-[390px] flex flex-col gap-5">
+            <h2 className="text-4xl font-bold text-shadow">SEO and digital marketing</h2>
+            <p className="text-2xl font-bold">
+              Optimize your web presence to rank higher on search engines and
+              boost traffic.
+            </p>
+            <div className="flex justify-center mt-10">
+              <Link href='/services/portfolio' className="flex flex-row gap-5 px-5 py-3 items-center justify-center bg-primary rounded-md">
+                <span className="text-2xl">Learn more</span>
+                <MoveUpRight size={32} />
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from 'tailwindcss/plugin'
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,8 +15,24 @@ module.exports = {
       },
       backgroundImage : {
         'hero-pattern' : "url('/bg-home.png')"
-      }
+      },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 };
