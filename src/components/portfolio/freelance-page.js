3a75d9b817aa3a3a2f097df58f8c0a3127ea5d43
hakeemsalman <salman74163@gmail.com>
2024-11-21 13:10:21 +0530

    fix apos

--- src/components/portfolio/freelance-page.js
+++ src/components/portfolio/freelance-page.js
@@ -10,7 +10,7 @@ export default function FreelancePage() {
       <div className='flex flex-row-reverse  items-center justify-between px-0 md:px-10 2xl:px-60'>
         <div className='flex flex-col gap-5 text-start w-1/2 max-w-xl'>
           <p className={`${istokWeb.className} text-5xl font-bold leading-snug`}>Why Choose Talatech Freelance Collective?</p>
-          <p className={`${manrope.className} text-2xl leading-10`}>We don't just complete projects-we partner with you for long-term success, providing seamless integration with your existing operations and ensuring your unique needs are met. Our collective of experienced freelancers works efficiently, so you get top-tier results every time.</p>
+          <p className={`${manrope.className} text-2xl leading-10`}>We don&apos;t just complete projects-we partner with you for long-term success, providing seamless integration with your existing operations and ensuring your unique needs are met. Our collective of experienced freelancers works efficiently, so you get top-tier results every time.</p>
         </div>
         <div>
           <Image src='/thumbnail/freelance-development.png' width={400} height={600} alt='core services thumbnail' />
