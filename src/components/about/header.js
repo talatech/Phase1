57215b98ac6b37c299c6727320438c4805e3532d
Preston <66348031+prest626@users.noreply.github.com>
2024-12-02 10:05:53 -0600

    Update header.js
    Adding the text to the who are we in talatech

--- src/components/about/header.js
+++ src/components/about/header.js
@@ -8,7 +8,7 @@ export default function Header() {
         <Image src='/thumbnail/about-tb-1.png' width={385} height={351} alt='about-image' />
         <div className='flex flex-col gap-8 max-w-md'>
           <h2 className='text-6xl font-bold text-white'>Who we are</h2>
-          <p className='text-white text-3xl'>Lorem ipsum Ull Cillum tempor adipisicing labore consequat commodo ex nisi. Nulla pariatur nulla fugiat nulla officia dolor laboris.</p>
+          <p className='text-white text-3xl'>At TalaTech, we specialize in Website Performance Audits, Full Stack Web Development, SEO, and Design. Whether you need quick assessments or comprehensive solutions, our team helps businesses overcome challenges, seize opportunities, and drive measurable growth.</p>
         </div>
       </div>
     </div>
